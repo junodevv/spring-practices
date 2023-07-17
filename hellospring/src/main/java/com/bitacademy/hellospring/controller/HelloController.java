0fa92c01ded7b52cce9877ca6671ed9b761b7fd2
leejuno <zhqkfxm0127@naver.com>
2023-07-17 15:00:49 +0900

    add: hello3.jsp추가, 파라메터받는법, 매핑

--- hellospring/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/bitacademy/hellospring/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.bitacademy.hellospring.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /*어노테이션(@)은 사전적 의미로는 주석이라는 뜻이다.
 자바에서 사용될 때의 어노테이션은 코드 사이에 주석처럼 쓰여서 특별한 의미, 기능을 수행하도록 하는 기술이다. 
@@ -14,4 +15,19 @@ public class HelloController {
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+	
+	@RequestMapping("/hello2")
+	public String hello2(String name) {// request중 parameter가 name인 애를 찾아서 넣음
+		System.out.println("name: " + name);
+		return "/WEB-INF/views/hello.jsp";
+	}
+	
+	@RequestMapping("/hello3")
+	public ModelAndView hello3(String name) {// request중 parameter가 name인 애를 찾아서 넣음
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		return mav;
+	}
+	
 }
